f322c54ff6568a2b0b9457f824329fd9c47e9475
wallsieiswallsie <ralazinawla@gmail.com>
2025-10-15 00:28:05 +0700

    update database connection for Railway

--- knexfile.js
+++ knexfile.js
@@ -3,13 +3,19 @@ require("dotenv").config();
 module.exports = {
   development: {
     client: "pg",
-    connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      port: process.env.DB_PORT,
+    connection: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+    migrations: {
+      directory: "./migrations",
     },
+    seeds: {
+      directory: "./seeds",
+    },
+  },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
     migrations: {
       directory: "./migrations",
     },
--- src/db.js
+++ src/db.js
@@ -3,13 +3,8 @@ const knex = require("knex");
 
 const db = knex({
   client: "pg",
-  connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-  },
+  connection: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
 
 module.exports = db;
