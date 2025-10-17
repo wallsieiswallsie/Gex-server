3464bb3a8e5f581e732938b942f7e8ced4feefdc
wallsieiswallsie <ralazinawla@gmail.com>
2025-10-17 19:40:37 +0700

    add finance_deposits

--- migrations/20251017120735_finance_deposits.js
+++ migrations/20251017120735_finance_deposits.js
@@ -1,23 +1,18 @@
 export function up(knex) {
   return knex.schema.createTable("finance_deposits", (table) => {
-    table
-      .uuid("id")
-      .primary()
-      .defaultTo(knex.raw("gen_random_uuid()"));
-
+    table.increments("id").primary();
     table.decimal("nominal", 15, 2).notNullable();
     table.timestamp("tanggal_setoran").defaultTo(knex.fn.now());
     table.text("keterangan").nullable();
 
     table
-      .uuid("created_by")
+      .integer("created_by")
+      .unsigned()
       .notNullable()
       .references("id")
       .inTable("users")
-      .onDelete("RESTRICT")
-      .onUpdate("CASCADE");
-
-    table.index("created_by");
+      .onUpdate("CASCADE")
+      .onDelete("RESTRICT");
   });
 }
 
